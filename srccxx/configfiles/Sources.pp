c43cf7f2b34035b506413da4f413ce10eed0fe3e
Brian Lach <brianlach72@gmail.com>
2022-12-08 17:35:13 -0500

    remove srccxx
    I considered rewriting the entire game in C++, but I went back on that

--- srccxx/configfiles/Sources.pp
+++
@@ -1,9 +0,0 @@
-#define INSTALL_CONFIG \
-  50_tf.prc
-
-#if $[CTPROJS]
-  // These files only matter to ctattach users.
-  #define INSTALL_CONFIG $[INSTALL_CONFIG] tf.init
-#endif
-
-#include $[THISDIRPREFIX]tf.prc.pp
