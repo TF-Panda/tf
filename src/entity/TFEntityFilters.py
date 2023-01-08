10defc009b0b91432c44138c7230b89b743b559d
Brian Lach <brianlach72@gmail.com>
2023-01-08 15:02:59 -0500

    remove some debug prints

--- src/entity/TFEntityFilters.py
+++ src/entity/TFEntityFilters.py
@@ -19,13 +19,9 @@ class FilterActivatorTFTeam(FilterBase):
         if not activator.isPlayer():
             return False
 
-        print("test", activator, "against filter_activator_tfteam")
-        print(activator.team, self.teamNum)
-
         if base.game.isRoundEnded() and not base.game.isStalemate():
             # If it's not a stalemate, we allow the winning team and the
             # set team.
-            print("in non-stalemate round end, allowing win team as well")
             return activator.team in (self.teamNum, base.game.winTeam)
         # Otherwise, we only allow the set team.
         return activator.team == self.teamNum
